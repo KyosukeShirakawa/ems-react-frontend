db85b52a97091e3d5aef4b162413e900f8a693e3
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-02-07 08:21:39 +0100

    creating header and footer

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <footer className='footer'>
+      <span>All rights reserved 2023 by kyosuke shirakawa</span>
+    </footer>
+  )
+}
+
+export default FooterComponent
