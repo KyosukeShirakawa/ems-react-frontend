3786718c1947bbd93371d997ef0bf914a3ffc90a
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-02-07 20:41:55 +0100

    implementing get employee by id

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -9,6 +9,10 @@ export const createEmployee = (employee) => {
   return axios.post(REST_API_BASE_URL, employee);
 }
 
+export const getEmployee = (id) => {
+  return axios.get(REST_API_BASE_URL+ '/'+ id);
+}
+
 export const updateEmployee = (employee) => {
   return axios.put(REST_API_BASE_URL, employee);
 }
